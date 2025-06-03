3bb7af35bcda5ec4690b85c3296a57fd8e2c649e
Jesús López <jesale.lopro@gmail.com>
2025-06-03 08:00:23 -0600

    fix: actualizar iconos de la aplicación a formato SVG

--- vite.config.ts
+++ vite.config.ts
@@ -19,17 +19,17 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: '/emotion-icon-192.png',
+            src: '/emotion-icon-192.svg',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/emotion-icon-512.png',
+            src: '/emotion-icon-512.svg',
             sizes: '512x512',
             type: 'image/png'
           },
           {
-            src: '/emotion-icon-512.png',
+            src: '/emotion-icon-512.svg',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable'
